4e1b3ae1b4ab3dce07dfbd794a6eed0ce3bfdabc
Casey Brown <casey.92.brown@gmail.com>
2018-10-10 21:48:59 -0700

    updated some methods

--- Number Printer/ConsoleApp1/Module1.vb
+++ Number Printer/ConsoleApp1/Module1.vb
@@ -3,109 +3,91 @@ Imports System.IO
 
 Module Module1
     Dim answer As String
+    Dim objDoc As bpac.Document
+
+    Sub PrintLabel(ByVal newLabelText As String, ByVal copiesToPrint As Integer)
+        objDoc.GetObject("Text2").Text = newLabelText
+        objDoc.StartPrint("", bpac.PrintOptionConstants.bpoDefault)
+        objDoc.PrintOut(copiesToPrint, bpac.PrintOptionConstants.bpoDefault)
+    End Sub
 
 
     Sub Main()
 
-        Dim x As Integer
-        Dim stopNum As Integer
-        Dim i As Integer
+        Dim beginNum As Integer
+        Dim numsToPrint As Integer
         Dim copies As Integer
-        Dim spath As String
-        Dim objDoc As bpac.Document
+        Dim labelTemplatePath As String = ""
+
         Dim bRet As Boolean
         Dim ans As String
-        Dim one As String = ""
-        Dim two As String = ""
-        Dim three As String = ""
+        Dim arg1 As String = ""
+        Dim arg2 As String = ""
+        Dim arg3 As String = ""
         Dim c As String()
 
 
         objDoc = New bpac.Document
-        bRet = objDoc.Open(spath)
-
-        objDoc = New bpac.Document
-        bRet = objDoc.Open(spath)
+        bRet = objDoc.Open(labelTemplatePath)
 
         Console.WriteLine("Do you want to print a string attached to your number? Enter Y/N")
         ans = Console.ReadLine().ToUpper
+
         If ans = "Y" Then
-            spath = My.Application.Info.DirectoryPath & "\count2.lbx"
-            Console.WriteLine("Please enter in up to 3 characters seperated by a ,      example: A,B,C")
-            ans = Console.ReadLine()
-            c = ans.Split(",")
+            labelTemplatePath = My.Application.Info.DirectoryPath & "\count2.lbx"
+
+            Console.WriteLine("Please enter in up to 3 characters seperated by a , example: A,B,C")
+            c = Console.ReadLine().Split(",")
+
             If c.Count = 1 Then
-                one = c(0)
+                arg1 = c(0)
             ElseIf c.Count = 2 Then
-                one = c(0)
-                two = c(1)
+                arg1 = c(0)
+                arg2 = c(1)
             ElseIf c.Count = 3 Then
-                one = c(0)
-                two = c(1)
-                three = c(2)
+                arg1 = c(0)
+                arg2 = c(1)
+                arg3 = c(2)
             End If
         Else
-            spath = My.Application.Info.DirectoryPath & "\count.lbx"
+            labelTemplatePath = My.Application.Info.DirectoryPath & "\count.lbx"
         End If
 
         Console.WriteLine("What number do you want to start at?")
-        x = Console.ReadLine()
-        stopNum = x
+        beginNum = Console.ReadLine()
 
         Console.WriteLine("How many numbers do you want to print?")
-        i = Console.ReadLine()
+        numsToPrint = Console.ReadLine() + beginNum
 
         Console.WriteLine("How many copies of each number do you need?")
         copies = Console.ReadLine()
 
 
-        If (objDoc.Open(spath) <> False) Then
-            While x < stopNum + i
-
-
+        If (objDoc.Open(labelTemplatePath) <> False) Then
+            While beginNum < numsToPrint
                 If ans = "N" Then
-                    objDoc.GetObject("Text2").Text = x
-                    objDoc.StartPrint("", bpac.PrintOptionConstants.bpoDefault)
-                    objDoc.PrintOut(copies, bpac.PrintOptionConstants.bpoDefault)
-                ElseIf Not one = "" And two = "" Then
-                    objDoc.GetObject("Text2").Text = x & one
-                    objDoc.StartPrint("", bpac.PrintOptionConstants.bpoDefault)
-                    objDoc.PrintOut(copies, bpac.PrintOptionConstants.bpoDefault)
-                ElseIf Not one = "" And Not two = "" And three = "" Then
-                    objDoc.GetObject("Text2").Text = x & one
-                    objDoc.StartPrint("", bpac.PrintOptionConstants.bpoDefault)
-                    objDoc.PrintOut(copies, bpac.PrintOptionConstants.bpoDefault)
+                    PrintLabel(beginNum, copies)
+                Else
+                    If arg1.Length > 0 Then
+                        PrintLabel(beginNum & arg1, copies)
+                    End If
 
-                    objDoc.GetObject("Text2").Text = x & two
-                    objDoc.StartPrint("", bpac.PrintOptionConstants.bpoDefault)
-                    objDoc.PrintOut(copies, bpac.PrintOptionConstants.bpoDefault)
+                    If arg2.Length > 0 Then
+                        PrintLabel(beginNum & arg2, copies)
+                    End If
 
-                ElseIf Not one = "" And Not two = "" And Not three = "" Then
-                    objDoc.GetObject("Text2").Text = x & one
-                    objDoc.StartPrint("", bpac.PrintOptionConstants.bpoDefault)
-                    objDoc.PrintOut(copies, bpac.PrintOptionConstants.bpoDefault)
+                    If arg3.Length > 0 Then
+                        PrintLabel(beginNum & arg3, copies)
+                    End If
 
-                    objDoc.GetObject("Text2").Text = x & two
-                    objDoc.StartPrint("", bpac.PrintOptionConstants.bpoDefault)
-                    objDoc.PrintOut(copies, bpac.PrintOptionConstants.bpoDefault)
-
-                    objDoc.GetObject("Text2").Text = x & three
-                    objDoc.StartPrint("", bpac.PrintOptionConstants.bpoDefault)
-                    objDoc.PrintOut(copies, bpac.PrintOptionConstants.bpoDefault)
-
+                    beginNum += 1
                 End If
-
-
-                x += 1
             End While
-            'objDoc.Close()
+        Else
+            Throw New Exception("File not found: " + labelTemplatePath)
         End If
 
-
-
-
+        'Clean up resources
+        objDoc.Close()
     End Sub
-
-
-
 End Module
